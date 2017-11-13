931a1110d4fe3de10165d5c68863c3c03a58c37f
zloedi <stoiko.todorov@gmail.com>
2017-11-13 16:09:04 +0200

    Pick a figure at random.

--- main.c
+++ main.c
@@ -20,12 +20,13 @@ static void NarisuvaiSimvol( c2_t position, int symbol ) {
     R_BlendPicV2( v2xy( position.x * scale.x, position.y * scale.y ), scale, st0, st1, ASCIITexture );
 }
 
-static void InicializiraiAktivnaFigura( void );
+static void PickFigureAndReset( void );
 
 static void Init( void ) {
+    COM_SRand( SDL_GetTicks() );
     ASCIITexture = R_LoadStaticTextureEx( "cp437_12x12.png", &ASCIITextureSize );
     ASCIISymbolSize = v2Scale( ASCIITextureSize, 1 / 16. );
-    InicializiraiAktivnaFigura();
+    PickFigureAndReset();
 }
 
 //==============================================================================================
@@ -68,8 +69,8 @@ Tue Sep 12 16:16:19 EEST 2017
 */
 
 // VNIMANIE! promeni tezi razmeri, ako promenish razmera na poleto!
-const c2_t IgralnoPoleRazmer = { .x = 12, .y = 20 };
-char IgralnoPole[] =
+const c2_t x_boardSize = { .x = 12, .y = 20 };
+char x_board[] =
 "#          #"
 "#          #"
 "#          #"
@@ -92,42 +93,19 @@ char IgralnoPole[] =
 "############"
 ;
 
-const c2_t FigChertaRazmer = { .x = 4, .y = 4 };
-const char *FigCherta =
-" @  "
-" @  "
-" @  "
-" @  "
-
-"    "
-"@@@@"
-"    "
-"    "
-;
-
-const c2_t FigZRazmer = { .x = 3, .y = 3 };
-const char *FigZ =
-"## "
-" ##"
-"   "
-
-" # "
-"## "
-"#  "
-;
-
 typedef struct {
-    c2_t razmer;
-    const char *karti[4];
-    int aktivnaKarta;
-    int broiKarti;
-} figura_t;
+    c2_t size;
+    int numBitmaps;
+    const char *bitmaps[4];
+    int curBitmap;
+} figure_t;
 
-static figura_t x_figuri[] = {
+static int x_currentFigure;
+static figure_t x_figures[] = {
     {
-        .razmer = { .x = 4, .y = 4 },
-        .broiKarti = 2,
-        .karti = {
+        .size = { .x = 4, .y = 4 },
+        .numBitmaps = 2,
+        .bitmaps = {
             " @  "
             " @  "
             " @  "
@@ -140,9 +118,9 @@ static figura_t x_figuri[] = {
         },
     },
     {
-        .razmer = { .x = 3, .y = 3 },
-        .broiKarti = 2,
-        .karti = {
+        .size = { .x = 3, .y = 3 },
+        .numBitmaps = 2,
+        .bitmaps = {
             "   "
             "@@ "
             " @@"
@@ -153,9 +131,9 @@ static figura_t x_figuri[] = {
         },
     },
     {
-        .razmer = { .x = 3, .y = 3 },
-        .broiKarti = 2,
-        .karti = {
+        .size = { .x = 3, .y = 3 },
+        .numBitmaps = 2,
+        .bitmaps = {
             "   "
             " @@"
             "@@ "
@@ -166,17 +144,17 @@ static figura_t x_figuri[] = {
         },
     },
     {
-        .razmer = { .x = 2, .y = 2 },
-        .broiKarti = 1,
-        .karti = {
+        .size = { .x = 2, .y = 2 },
+        .numBitmaps = 1,
+        .bitmaps = {
             "@@"
             "@@"
         },
     },
     {
-        .razmer = { .x = 3, .y = 3 },
-        .broiKarti = 3,
-        .karti = {
+        .size = { .x = 3, .y = 3 },
+        .numBitmaps = 3,
+        .bitmaps = {
             "   "
             "@@@"
             " @ "
@@ -195,9 +173,9 @@ static figura_t x_figuri[] = {
         },
     },
     {
-        .razmer = { .x = 3, .y = 3 },
-        .broiKarti = 4,
-        .karti = {
+        .size = { .x = 3, .y = 3 },
+        .numBitmaps = 4,
+        .bitmaps = {
             " @@"
             " @ "
             " @ "
@@ -216,9 +194,9 @@ static figura_t x_figuri[] = {
         },
     },
     {
-        .razmer = { .x = 3, .y = 3 },
-        .broiKarti = 4,
-        .karti = {
+        .size = { .x = 3, .y = 3 },
+        .numBitmaps = 4,
+        .bitmaps = {
             "@@ "
             " @ "
             " @ "
@@ -238,83 +216,75 @@ static figura_t x_figuri[] = {
     },
 };
 
-static const int x_broiFiguri = sizeof( x_figuri ) / sizeof( *x_figuri );
+static const int x_numFigures = sizeof( x_figures ) / sizeof( *x_figures );
 
-static c2_t x_poziciaNaAktivnataFigura;
+static c2_t x_currentPos;
 static int x_predishnoVreme;
-static int x_skorostNaAktivnaFigura = 64;
-static int x_butonNadolu;
+static int x_speed = 64;
+static int x_buttonDown;
 
-static int ProchetiSimvolOtKarta( c2_t poziciaVavFigura, c2_t razmerNaFigura, const char *figura ) {
-    if ( poziciaVavFigura.x >= 0 && poziciaVavFigura.x < razmerNaFigura.x
-        && poziciaVavFigura.y >= 0 && poziciaVavFigura.y < razmerNaFigura.y ) {
-        return figura[poziciaVavFigura.x + poziciaVavFigura.y * razmerNaFigura.x];
+static int ReadTile( c2_t pos, const char *bmp, c2_t bmpSz ) {
+    if ( pos.x >= 0 && pos.x < bmpSz.x
+        && pos.y >= 0 && pos.y < bmpSz.y ) {
+        return bmp[pos.x + pos.y * bmpSz.x];
     }
     return ' ';
 }
 
-static int ProchetiSimvolOtIgralnoPole( c2_t pozicia ) {
-    return ProchetiSimvolOtKarta( pozicia, IgralnoPoleRazmer, IgralnoPole );
+static int ReadTileOnBoard( c2_t pozicia ) {
+    return ReadTile( pozicia, x_board, x_boardSize );
 }
 
-static void ZapishiSimvolVKarta( int simvol, c2_t pozicia, c2_t razmerNaFigura, char *figura )
+static void CopyTile( int tile, c2_t pos, char *bmp, c2_t bmpSz )
 {
-    if ( pozicia.x >= 0 && pozicia.x < razmerNaFigura.x
-        && pozicia.y >= 0 && pozicia.y < razmerNaFigura.y ) {
-        figura[pozicia.x + pozicia.y * razmerNaFigura.x] = simvol;
+    if ( pos.x >= 0 && pos.x < bmpSz.x
+        && pos.y >= 0 && pos.y < bmpSz.y ) {
+        bmp[pos.x + pos.y * bmpSz.x] = tile;
     }
 }
 
-static bool_t EProzrachenSimvol( int simvol ) {
-    return simvol == ' ';
+static bool_t IsBlank( int tile ) {
+    return tile == ' ';
 }
 
-static void KopiraiKartaVDrugaKarta( const char *iztochnik, c2_t razmerIztochnik, 
-                                        char *cel, c2_t razmerCel, c2_t poziciaVCelta ) {
-    for ( int y = 0; y < razmerIztochnik.y; y++ ) {
-        for ( int x = 0; x < razmerIztochnik.x; x++ ) {
+static void CopyBitmap( const char *src, c2_t srcSz, 
+                            char *dst, c2_t dstSz, c2_t posInDst ) {
+    for ( int y = 0; y < srcSz.y; y++ ) {
+        for ( int x = 0; x < srcSz.x; x++ ) {
             c2_t xy = c2xy( x, y );
-            int simvol = ProchetiSimvolOtKarta( xy, razmerIztochnik, iztochnik );
-            if ( ! EProzrachenSimvol( simvol ) ) {
-                c2_t krainaPozicia = c2Add( xy, poziciaVCelta );
-                ZapishiSimvolVKarta( simvol, krainaPozicia, razmerCel, cel );
+            int tile = ReadTile( xy, src, srcSz );
+            if ( ! IsBlank( tile ) ) {
+                c2_t final = c2Add( xy, posInDst );
+                CopyTile( tile, final, dst, dstSz );
             }
         }
     }
 }
 
-static void NarisuvaiKartaOtSimvoli( c2_t poziciaNaEkrana, const char *karta, c2_t razmerNaKarta, color_t cviat ) {
-    static const int saotv[256] = {
+static void DrawBitmap( c2_t screenPos, const char *bitmap, c2_t razmerNaKarta, color_t cviat ) {
+    static const int remap[256] = {
         ['@'] = 1,
         ['#'] = 1 + 11 * 16,
     };
     R_ColorC( cviat );
     for ( int i = 0, y = 0; y < razmerNaKarta.y; y++ ) {
         for ( int x = 0; x < razmerNaKarta.x; x++ ) {
-            int simvol = karta[i++];
-            NarisuvaiSimvol( c2Add( c2xy( x, y ), poziciaNaEkrana ), saotv[simvol] );
+            int tile = bitmap[i++];
+            NarisuvaiSimvol( c2Add( c2xy( x, y ), screenPos ), remap[tile] );
         }
     }
 }
 
-static bool_t SekaLiIgralnoPole( c2_t poziciaNaFiguraNaEkrana, const char *figura, c2_t razmerNaFigura ) {
-    for ( int y = 0; y < razmerNaFigura.y; y++ ) {
-        for ( int x = 0; x < razmerNaFigura.x; x++ ) {
-            // tekushtia simvol ot figurata e na tazi pozicia
-            c2_t poziciaVavFigura = c2xy( x, y );
-            int simvolOtFigura = ProchetiSimvolOtKarta( poziciaVavFigura, razmerNaFigura, figura );
-            // otmesti s poziciaNaFiguraNaEkrana za da namerish poziciata na ekrana na tozi simvol
-            c2_t poziciaNaEkrana = c2Add( poziciaVavFigura, poziciaNaFiguraNaEkrana );
-            int simvolOtIgralnoPole = ProchetiSimvolOtIgralnoPole( poziciaNaEkrana );
-            // simvolat ot figurata e neprozrachen i simvolat na sashtata pozicia ot igralnoto pole e neprozrachen
-            // ! - otricanie
-            // == - ravenstvo
-            // != - neravenstvo
-            // && - logichesko "i"
-            // || - logichesko "ili"
-            if ( ! EProzrachenSimvol( simvolOtFigura ) 
-                    && ! EProzrachenSimvol( simvolOtIgralnoPole ) ) {
-                return true;
+static bool_t IsBitmapClipping( c2_t boardPos, const char *bmp, c2_t bmpSize ) {
+    for ( int y = 0; y < bmpSize.y; y++ ) {
+        for ( int x = 0; x < bmpSize.x; x++ ) {
+            c2_t pos = c2xy( x, y );
+            int tile = ReadTile( pos, bmp, bmpSize );
+            if ( ! IsBlank( tile ) ) {
+                int tileOnBoard = ReadTileOnBoard( c2Add( pos, boardPos ) );
+                if ( ! IsBlank( tileOnBoard ) ) {
+                    return true;
+                }
             }
         }
     }
@@ -329,68 +299,89 @@ static c2_t FixedToInt( c2_t c ) {
     return c2RShifts( c, 8 );
 }
 
-static void InicializiraiAktivnaFigura( void ) {
-    x_poziciaNaAktivnataFigura = IntToFixed( c2xy( IgralnoPoleRazmer.x / 2 - 2, -FigChertaRazmer.y + 1 ) );
+static void PickFigureAndReset( void ) {
+    x_currentFigure = COM_Rand() % x_numFigures;
+    figure_t *curFig = &x_figures[x_currentFigure];
+    x_currentPos = IntToFixed( c2xy( x_boardSize.x / 2 - 2, -curFig->size.y + 1 ) );
     x_predishnoVreme = SYS_RealTime();
-    x_butonNadolu = 0;
+    x_buttonDown = 0;
 }
 
-static bool_t ProveriIMesti( c2_t badeshtaPozicia ) {
-    bool_t mogaDaMestia = ! SekaLiIgralnoPole( FixedToInt( badeshtaPozicia ), FigCherta, FigChertaRazmer );
-    if ( mogaDaMestia ) {
-        x_poziciaNaAktivnataFigura = badeshtaPozicia;
+static void GetCurrentBitmap( const char **outBmp, c2_t *outBmpSz ) {
+    figure_t *curFigure = &x_figures[x_currentFigure];
+    *outBmp = curFigure->bitmaps[curFigure->curBitmap];
+    *outBmpSz = curFigure->size;
+}
+
+static bool_t TryMove( c2_t nextPos ) {
+    const char *bmp;
+    c2_t bmpSz;
+    GetCurrentBitmap( &bmp, &bmpSz );
+    if ( IsBitmapClipping( FixedToInt( nextPos ), bmp, bmpSz ) ) {
+        return false;
     }
-    return mogaDaMestia;
-}
-    
-static bool_t MestiNadolu( int deltaVreme ) {
-    int y = x_poziciaNaAktivnataFigura.y + deltaVreme * x_skorostNaAktivnaFigura * ( 1 + x_butonNadolu * 3 ) / 100;
-    c2_t badeshtaPozicia = c2xy( x_poziciaNaAktivnataFigura.x, y );
-    return ProveriIMesti( badeshtaPozicia );
+    x_currentPos = nextPos;
+    return true;
 }
 
-static void PraviKadar( void ) {
+static bool_t TryMoveDown( int deltaTime ) {
+    int y = x_currentPos.y + deltaTime * x_speed * ( 1 + x_buttonDown * 3 ) / 100;
+    c2_t nextPos = c2xy( x_currentPos.x, y );
+    return TryMove( nextPos );
+}
+
+static void Stop( void ) {
+    const char *bmp;
+    c2_t bmpSz;
+    GetCurrentBitmap( &bmp, &bmpSz );
+    CopyBitmap( bmp, bmpSz, x_board, x_boardSize, FixedToInt( x_currentPos ) );
+}
+
+static void GameUpdate( void ) {
     int sega = SYS_RealTime();
     int deltaVreme = sega - x_predishnoVreme;
-    if ( ! MestiNadolu( deltaVreme ) ) {
-        KopiraiKartaVDrugaKarta( FigCherta, FigChertaRazmer, IgralnoPole, IgralnoPoleRazmer, FixedToInt( x_poziciaNaAktivnataFigura ) );
-        InicializiraiAktivnaFigura();
+    if ( ! TryMoveDown( deltaVreme ) ) {
+        Stop();
+        PickFigureAndReset();
     } else {
-        NarisuvaiKartaOtSimvoli( FixedToInt( x_poziciaNaAktivnataFigura ), FigCherta, FigChertaRazmer, colGreen );
+        const char *bmp;
+        c2_t bmpSz;
+        GetCurrentBitmap( &bmp, &bmpSz );
+        DrawBitmap( FixedToInt( x_currentPos ), bmp, bmpSz, colGreen );
     }
     x_predishnoVreme = sega;
 }
 
 static void MestiNadiasno_f( void ) {
-    c2_t badeshtaPozicia = c2Add( x_poziciaNaAktivnataFigura, IntToFixed( c2xy( +1, 0 ) ) );
-    ProveriIMesti( badeshtaPozicia );
+    c2_t nextPos = c2Add( x_currentPos, IntToFixed( c2xy( +1, 0 ) ) );
+    TryMove( nextPos );
 }
 
-static void MestiNaliavo_f( void ) {
-    c2_t badeshtaPozicia = c2Add( x_poziciaNaAktivnataFigura, IntToFixed( c2xy( -1, 0 ) ) );
-    ProveriIMesti( badeshtaPozicia );
+static void MoveLeft_f( void ) {
+    c2_t nextPos = c2Add( x_currentPos, IntToFixed( c2xy( -1, 0 ) ) );
+    TryMove( nextPos );
 }
 
-static void MestiNadolu_f( void ) {
-    x_butonNadolu = *CMD_Argv( 0 ) == '+' ? 1 : 0;
+static void MoveDown_f( void ) {
+    x_buttonDown = *CMD_Argv( 0 ) == '+' ? 1 : 0;
 }
 
 static void RegistriraiKomandi( void ) {
-    CMD_Register( "mestiNaliavo", MestiNaliavo_f );
+    CMD_Register( "moveLeft", MoveLeft_f );
     CMD_Register( "mestiNadiasno", MestiNadiasno_f );
-    CMD_Register( "mestiNadolu", MestiNadolu_f );
-    I_Bind( "Left", "+mestiNaliavo" );
+    CMD_Register( "moveDown", MoveDown_f );
+    I_Bind( "Left", "+moveLeft" );
     I_Bind( "Right", "+mestiNadiasno" );
-    I_Bind( "Down", "mestiNadolu" );
+    I_Bind( "Down", "moveDown" );
 }
 
 static void AppFrame( void ) {
     // izchisli goleminata na simvolite/spraitove
     // simvoli i spraitove v tozi sa vzaimnozameniaemi
     PixelSize = Maxi( R_GetWindowSize().y / 320, 1 );
-    PraviKadar();
+    GameUpdate();
     // narisuvai igralnoto pole izpolzvaiki funkciata za risuvane na karta ot simvoli
-    NarisuvaiKartaOtSimvoli( c2zero, IgralnoPole, IgralnoPoleRazmer, colWhite );
+    DrawBitmap( c2zero, x_board, x_boardSize, colWhite );
     // narisuvai tablicata sas simvoli v desnia agal na ekrana
     v2_t windowSize = R_GetWindowSize();
     DrawASCIITexture( v2xy( windowSize.x - ASCIITextureSize.x, 0 ) );
