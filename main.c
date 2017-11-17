7eea1931e76829e8f733da036fae570fee5d2d65
zloedi <stoiko.todorov@gmail.com>
2017-11-17 21:25:52 +0200

    Controller support wip.

--- main.c
+++ main.c
@@ -56,7 +56,9 @@ Tue Sep 12 16:16:19 EEST 2017
 #define DBG_PRINT CON_Printf
 #include "zhost.h"
 
-const c2_t x_boardSize = { .x = 12, .y = 20 };
+#define BOARD_SIZE_X 12
+#define BOARD_SIZE_Y 20
+const c2_t x_boardSize = { .x = BOARD_SIZE_X, .y = BOARD_SIZE_Y };
 char x_board[] =
 "#          #"
 "#          #"
@@ -218,15 +220,40 @@ static Mix_Music *x_music;
 static Mix_Chunk *x_soundPop;
 static Mix_Chunk *x_soundThud;
 static Mix_Chunk *x_soundShift;
-static bool_t x_gameOver;
 static rImage_t *ASCIITexture;
 static v2_t ASCIITextureSize;
 static v2_t ASCIISymbolSize;
 static int PixelSize;
-static int x_currentShape;
-static int x_nextShape;
 static var_t *x_showAtlas;
 static var_t *x_hiscore;
+static var_t *x_musicVolume;
+static int x_prevTime;
+
+#define INITIAL_SPEED 40
+
+typedef enum {
+    BS_RELEASED,
+    BS_PRESSED,
+    BS_LATCHED,
+} butState_t;
+
+typedef struct {
+    bool_t gameOver;
+    int nextShape;
+    int currentShape;
+    int currentBitmap;
+    c2_t currentPos;
+    int speed;
+    butState_t butMoveDown;
+    butState_t butMoveLeft;
+    butState_t butMoveRight;
+    butState_t butRotate;
+    int numErasedLines;
+    int score;
+    char board[BOARD_SIZE_X * BOARD_SIZE_Y];
+} playerSeat_t;
+
+static playerSeat_t x_pls;
 
 static void DrawAtlas( void ) {
     v2_t windowSize = R_GetWindowSize();
@@ -254,19 +281,54 @@ static void DrawTile( c2_t position, int symbol, color_t color ) {
     DrawTileKern( position, symbol, color, 1, 0 );
 }
 
-static void PickShapeAndReset( void );
+static int GetRandomShape( void ) {
+    return COM_Rand() % x_numShapes;
+}
+
+static c2_t IntToFixed( c2_t c ) {
+    return c2LShifts( c, 8 );
+}
+
+static c2_t FixedToInt( c2_t c ) {
+    return c2RShifts( c, 8 );
+}
+
+static void LatchButtons( playerSeat_t *pls ) {
+    pls->butMoveDown = pls->butMoveDown == BS_PRESSED ? BS_LATCHED : BS_RELEASED;
+    pls->butMoveLeft = pls->butMoveLeft == BS_PRESSED ? BS_LATCHED : BS_RELEASED;
+    pls->butMoveRight = pls->butMoveRight == BS_PRESSED ? BS_LATCHED : BS_RELEASED;
+    pls->butRotate = pls->butRotate == BS_PRESSED ? BS_LATCHED : BS_RELEASED;
+}
+
+static void PickShapeAndReset( playerSeat_t *pls ) {
+    pls->currentBitmap = 0;
+    pls->currentShape = x_pls.nextShape;
+    pls->nextShape = GetRandomShape();
+    pls->currentPos = IntToFixed( c2xy( x_boardSize.x / 2 - 2, -x_shapeSize.y + 1 ) );
+}
 
 static const char* GetAssetPath( const char *name ) {
     return va( "%sdata/%s", SYS_BaseDir(), name );
 }
 
-static int GetRandomShape( void ) {
-    return COM_Rand() % x_numShapes;
+static void StartNewGame( playerSeat_t *pls ) {
+    pls->gameOver = false;
+    pls->speed = INITIAL_SPEED;
+    pls->numErasedLines = 0;
+    pls->nextShape = GetRandomShape();
+    pls->score = 0;
+    memcpy( pls->board, x_board, sizeof( x_board ) );
+    PickShapeAndReset( pls );
+}
+
+static void UpdateMusicVolume( void ) {
+    float vol = Clampf( VAR_Num( x_musicVolume ), 0, 1 );
+    Mix_VolumeMusic( vol * MIX_MAX_VOLUME / 4 );
 }
 
 static void Init( void ) {
     x_music = Mix_LoadMUS( GetAssetPath( "ievan_polkka_8bit.ogg" ) );
-    Mix_VolumeMusic( MIX_MAX_VOLUME / 4 );
+    UpdateMusicVolume();
     x_soundPop = Mix_LoadWAV( GetAssetPath( "pop.ogg" ) );
     Mix_VolumeChunk( x_soundPop, MIX_MAX_VOLUME / 4 );
     x_soundThud = Mix_LoadWAV( GetAssetPath( "thud.ogg" ) );
@@ -274,19 +336,10 @@ static void Init( void ) {
     Mix_VolumeChunk( x_soundShift, MIX_MAX_VOLUME / 2 );
     ASCIITexture = R_LoadStaticTextureEx( "cp437_12x12.png", &ASCIITextureSize );
     ASCIISymbolSize = v2Scale( ASCIITextureSize, 1 / 16. );
-    x_nextShape = GetRandomShape();
-    PickShapeAndReset();
+    x_prevTime = SYS_RealTime();
     Mix_PlayMusic( x_music, -1 );
+    StartNewGame( &x_pls );
 }
-
-static int x_currentBitmap;
-static c2_t x_currentPos;
-static int x_prevTime;
-#define INITIAL_SPEED 40
-static int x_speed = INITIAL_SPEED;
-static int x_buttonDown;
-static int x_numErasedLines;
-static int x_score;
 
 static int ReadTile( c2_t pos, const char *bmp, c2_t bmpSz ) {
     if ( pos.x >= 0 && pos.x < bmpSz.x
@@ -296,11 +349,11 @@ static int ReadTile( c2_t pos, const char *bmp, c2_t bmpSz ) {
     return ' ';
 }
 
-static int ReadTileOnBoard( c2_t pos ) {
+static int ReadTileOnBoard( const char *board, c2_t pos ) {
     if ( pos.x <= 0 || pos.x >= x_boardSize.x - 1 ) {
         return '#';
     }
-    return ReadTile( pos, x_board, x_boardSize );
+    return ReadTile( pos, board, x_boardSize );
 }
 
 static void CopyTile( int tile, c2_t pos, char *bmp, c2_t bmpSz )
@@ -352,13 +405,13 @@ static void Print( c2_t pos, const char *string, color_t color ) {
     }
 }
 
-static bool_t IsBitmapClipping( c2_t posOnBoard, const char *bmp ) {
+static bool_t IsBitmapClipping( const char *board, c2_t posOnBoard, const char *bmp ) {
     for ( int y = 0; y < x_shapeSize.y; y++ ) {
         for ( int x = 0; x < x_shapeSize.x; x++ ) {
             c2_t pos = c2xy( x, y );
             int tile = ReadTile( pos, bmp, x_shapeSize );
             if ( ! IsBlank( tile ) ) {
-                int tileOnBoard = ReadTileOnBoard( c2Add( pos, posOnBoard ) );
+                int tileOnBoard = ReadTileOnBoard( board, c2Add( pos, posOnBoard ) );
                 if ( ! IsBlank( tileOnBoard ) ) {
                     return true;
                 }
@@ -368,72 +421,56 @@ static bool_t IsBitmapClipping( c2_t posOnBoard, const char *bmp ) {
     return false;
 }
 
-static c2_t IntToFixed( c2_t c ) {
-    return c2LShifts( c, 8 );
-}
-
-static c2_t FixedToInt( c2_t c ) {
-    return c2RShifts( c, 8 );
-}
-
 static const char* GetCurrentBitmap( void ) {
-    shape_t *curShape = &x_shapes[x_currentShape];
-    return curShape->bitmaps[x_currentBitmap];
+    shape_t *curShape = &x_shapes[x_pls.currentShape];
+    return curShape->bitmaps[x_pls.currentBitmap];
 }
 
-static bool_t IsCurrentBitmapClipping( c2_t fixedPointPos ) {
-    return IsBitmapClipping( FixedToInt( fixedPointPos ), GetCurrentBitmap() );
+static bool_t IsCurrentBitmapClipping( const char *board, c2_t fixedPointPos ) {
+    return IsBitmapClipping( board, FixedToInt( fixedPointPos ), GetCurrentBitmap() );
 }
 
-static void PickShapeAndReset( void ) {
-    x_currentBitmap = 0;
-    x_currentShape = x_nextShape;
-    x_nextShape = GetRandomShape();
-    x_currentPos = IntToFixed( c2xy( x_boardSize.x / 2 - 2, -x_shapeSize.y + 1 ) );
-    x_prevTime = SYS_RealTime();
-    x_buttonDown = x_buttonDown ? -1 : 0;
-}
-
-static bool_t TryMove( c2_t nextPos ) {
-    if ( IsCurrentBitmapClipping( nextPos ) ) {
+static bool_t TryMove( const char *board, c2_t nextPos ) {
+    if ( IsCurrentBitmapClipping( board, nextPos ) ) {
         return false;
     }
-    x_currentPos = nextPos;
+    x_pls.currentPos = nextPos;
     return true;
 }
 
-static bool_t TryMoveDown( int deltaTime ) {
-    int y = x_currentPos.y + deltaTime * x_speed * ( 1 + Maxi( x_buttonDown, 0 ) * 6 ) / 100;
-    c2_t nextPos = c2xy( x_currentPos.x, y );
-    return TryMove( nextPos );
+static bool_t TryMoveDown( const char *board, int deltaTime ) {
+    int butMove = x_pls.butMoveDown == BS_PRESSED;
+    int y = x_pls.currentPos.y + deltaTime * x_pls.speed * ( 1 + butMove * 6 ) / 100;
+    c2_t nextPos = c2xy( x_pls.currentPos.x, y );
+    return TryMove( board, nextPos );
 }
 
-static bool_t Drop( void ) {
-    CopyBitmap( GetCurrentBitmap(), x_shapeSize, x_board, x_boardSize, FixedToInt( x_currentPos ) );
+static bool_t Drop( playerSeat_t *pls ) {
+    CopyBitmap( GetCurrentBitmap(), x_shapeSize, pls->board, x_boardSize, FixedToInt( pls->currentPos ) );
     Mix_PlayChannel( -1, x_soundThud, 0 );
-    x_score += x_speed / 2;
-    return x_currentPos.y + x_shapeSize.y >= 0;
+    pls->score += pls->speed / 2;
+    return pls->currentPos.y + x_shapeSize.y >= 0;
 }
 
-static void EraseFilledLines( void ) {
+static void EraseFilledLines( playerSeat_t *pls ) {
     int bonus = 1;
     bool_t result = false;
     for ( int y = x_boardSize.y - 2; y >= 1; ) {
         int numFull = 0;
         for ( int x = 0; x < x_boardSize.x; x++ ) {
-            int tile = ReadTile( c2xy( x, y ), x_board, x_boardSize );
+            int tile = ReadTile( c2xy( x, y ), pls->board, x_boardSize );
             if ( ! IsBlank( tile ) ) {
                 numFull++;
             }
         }
         if ( numFull == x_boardSize.x ) {
             result = true;
-            x_numErasedLines++;
+            pls->numErasedLines++;
             bonus *= 2;
-            CON_Printf( "Pop a line. TOTAL LINES: %d\n", x_numErasedLines );
+            CON_Printf( "Pop a line. TOTAL LINES: %d\n", pls->numErasedLines );
             for ( int i = y; i >= 1; i-- ) {
-                char *dst = &x_board[( i - 0 ) * x_boardSize.x];
-                char *src = &x_board[( i - 1 ) * x_boardSize.x];
+                char *dst = &pls->board[( i - 0 ) * x_boardSize.x];
+                char *src = &pls->board[( i - 1 ) * x_boardSize.x];
                 memcpy( dst, src, x_boardSize.x );
             }
         } else {
@@ -441,120 +478,153 @@ static void EraseFilledLines( void ) {
         }
     }
     if ( result ) {
-        x_score += bonus * x_speed * 4;
+        pls->score += bonus * pls->speed * 4;
         Mix_PlayChannel( -1, x_soundPop, 0 );
     }
+}
+
+static void TryMoveHorz( const char *board, int deltaTime ) {
+    int dir = ( x_pls.butMoveRight == BS_PRESSED ) 
+            - ( x_pls.butMoveLeft == BS_PRESSED );
+    if ( dir == 0 ) {
+        return;
+    }
+    int off = dir > 0 ? deltaTime : -deltaTime;
+    c2_t nextPos = c2Add( x_pls.currentPos, c2xy( off, 0 ) );
+    TryMove( board, nextPos );
 }
 
 static void GameUpdate( void ) {
     int now = SYS_RealTime();
     int deltaTime = now - x_prevTime;
-    if ( x_gameOver ) {
-        if ( VAR_Num( x_hiscore ) < x_score ) {
-            VAR_Set( x_hiscore, va( "%d", x_score ), false );
+    if ( x_pls.gameOver ) {
+        if ( VAR_Num( x_hiscore ) < x_pls.score ) {
+            VAR_Set( x_hiscore, va( "%d", x_pls.score ), false );
         }
-        DrawBitmap( c2zero, x_board, x_boardSize, colWhite );
+        DrawBitmap( c2zero, x_pls.board, x_boardSize, colWhite );
         Print( c2xy( x_boardSize.x / 2 - 3, x_boardSize.y / 2 - 1 ), "GAME OVER", colRed );
         if ( now & 256 ) { 
             Print( c2xy( x_boardSize.x / 2 - 4, x_boardSize.y / 2 ), "Press 'Space'", colRed );
         }
     } else {
-        if ( ! TryMoveDown( deltaTime ) ) {
-            if ( ! Drop() ) {
-                x_gameOver = true;
+        TryMoveHorz( x_pls.board, deltaTime );
+        if ( ! TryMoveDown( x_pls.board, deltaTime ) ) {
+            if ( ! Drop( &x_pls ) ) {
+                x_pls.gameOver = true;
                 Mix_HaltMusic();
             } else {
-                EraseFilledLines();
-                PickShapeAndReset();
-                if ( x_numErasedLines >= 10 ) {
+                EraseFilledLines( &x_pls );
+                PickShapeAndReset( &x_pls );
+                LatchButtons( &x_pls );
+                if ( x_pls.numErasedLines >= 10 ) {
                     CON_Printf( "SPEED UP!\n" );
-                    x_numErasedLines = 0;
-                    x_speed += x_speed / 4;
+                    x_pls.numErasedLines = 0;
+                    x_pls.speed += x_pls.speed / 4;
                 }
             }
         } else {
-            DrawBitmap( FixedToInt( x_currentPos ), GetCurrentBitmap(), x_shapeSize, colGreen );
+            DrawBitmap( FixedToInt( x_pls.currentPos ), GetCurrentBitmap(), x_shapeSize, colGreen );
         }
-        DrawBitmap( c2zero, x_board, x_boardSize, colWhite );
+        DrawBitmap( c2zero, x_pls.board, x_boardSize, colWhite );
     }
     Print( c2xy( x_boardSize.x + 1, 1 ), "NEXT", colCyan );
-    shape_t *nextShape = &x_shapes[x_nextShape];
+    shape_t *nextShape = &x_shapes[x_pls.nextShape];
     DrawBitmap( c2xy( x_boardSize.x + 1, 2 ), nextShape->bitmaps[0], x_shapeSize, colGreen );
     Print( c2xy( x_boardSize.x + 1, 7 ), "SCORE", colCyan );
-    Print( c2xy( x_boardSize.x + 1, 8 ), va( "%d", x_score ), colWhite );
+    Print( c2xy( x_boardSize.x + 1, 8 ), va( "%d", x_pls.score ), colWhite );
     Print( c2xy( x_boardSize.x + 1, 10 ), "HISCORE", colCyan );
     Print( c2xy( x_boardSize.x + 1, 11 ), va( "%d", ( int )VAR_Num( x_hiscore ) ), colWhite );
     x_prevTime = now;
 }
 
+static bool_t DoButton( bool_t down, butState_t *button ) {
+    bool_t result = false;
+    if ( down ) {
+        if ( *button != BS_PRESSED && *button != BS_LATCHED ) {
+            Mix_PlayChannel( -1, x_soundShift, 0 );
+            *button = BS_PRESSED;
+            result = true;
+        }
+    } else {
+        *button = BS_RELEASED;
+    }
+    return result;
+}
+
+static void DoRightButton( playerSeat_t *pls, bool_t down ) {
+    if ( DoButton( down, &pls->butMoveRight ) ) {
+        c2_t nextPos = c2xy( ( pls->currentPos.x & ~255 ) + 256, pls->currentPos.y );
+        TryMove( pls->board, nextPos );
+    }
+}
+
+static void DoLeftButton( playerSeat_t *pls, bool_t down ) {
+    if ( DoButton( down, &x_pls.butMoveLeft ) ) {
+        c2_t nextPos = c2xy( ( x_pls.currentPos.x & ~255 ), x_pls.currentPos.y );
+        TryMove( x_pls.board, nextPos );
+    }
+}
+
 static void MoveRight_f( void ) {
-    Mix_PlayChannel( -1, x_soundShift, 0 );
-    c2_t nextPos = c2Add( x_currentPos, IntToFixed( c2xy( +1, 0 ) ) );
-    TryMove( nextPos );
+    DoRightButton( &x_pls, *CMD_Argv( 0 ) == '+' );
 }
 
 static void MoveLeft_f( void ) {
-    Mix_PlayChannel( -1, x_soundShift, 0 );
-    c2_t nextPos = c2Add( x_currentPos, IntToFixed( c2xy( -1, 0 ) ) );
-    TryMove( nextPos );
+    DoLeftButton( &x_pls, *CMD_Argv( 0 ) == '+' );
+}
+
+static void HorzAxis_f( void ) {
+    int axis = atoi( CMD_Argv( 1 ) );
+    bool_t liveZone = abs( axis ) > 8000;
+    if ( liveZone ) {
+        if ( axis > 0 ) {
+            DoRightButton( &x_pls, true );
+        } else {
+            DoLeftButton( &x_pls, true );
+        }
+    } else {
+        DoRightButton( &x_pls, false );
+        DoLeftButton( &x_pls, false );
+    }
 }
 
 static void Rotate_f( void ) {
     Mix_PlayChannel( -1, x_soundShift, 0 );
-    shape_t *curShape = &x_shapes[x_currentShape];
-    int bmpIdx = ( x_currentBitmap + 1 ) % curShape->numBitmaps;
-    if ( ! IsBitmapClipping( FixedToInt( x_currentPos ), curShape->bitmaps[bmpIdx] ) ) {
-        x_currentBitmap = bmpIdx;
+    shape_t *curShape = &x_shapes[x_pls.currentShape];
+    int bmpIdx = ( x_pls.currentBitmap + 1 ) % curShape->numBitmaps;
+    if ( ! IsBitmapClipping( x_pls.board, FixedToInt( x_pls.currentPos ), curShape->bitmaps[bmpIdx] ) ) {
+        x_pls.currentBitmap = bmpIdx;
     }
 }
 
 static void MoveDown_f( void ) {
-    int down = *CMD_Argv( 0 ) == '+';
-    if ( down == x_buttonDown ) {
-        return;
-    }
-    if ( x_buttonDown == -1 && down ) {
-        // waiting for release
-        return;
-    }
-    if ( down ) {
-        Mix_PlayChannel( -1, x_soundShift, 0 );
-    }
-    x_buttonDown = down;
-}
-
-static void ClearBoard( void ) {
-    for ( int y = 0; y < x_boardSize.y - 1; y++ ) {
-        memset( &x_board[1 + y * x_boardSize.x], ' ', x_boardSize.x - 2 );
-    }
+    bool_t down = *CMD_Argv( 0 ) == '+';
+    DoButton( down, &x_pls.butMoveDown );
 }
 
 static void Restart_f( void ) {
-    if ( x_gameOver ) {
-        x_gameOver = false;
+    if ( x_pls.gameOver ) {
         Mix_PlayMusic( x_music, -1 );
-        x_speed = INITIAL_SPEED;
-        x_numErasedLines = 0;
-        x_nextShape = GetRandomShape();
-        x_score = 0;
-        ClearBoard();
-        PickShapeAndReset();
+        StartNewGame( &x_pls );
     }
 }
 
 static void RegisterVars( void ) {
     x_showAtlas = VAR_Register( "showAtlas", "0" );
     x_hiscore = VAR_Register( "hiscore", "0" );
+    x_musicVolume = VAR_Register( "musicVolume", "1" );
     CMD_Register( "moveLeft", MoveLeft_f );
     CMD_Register( "moveRight", MoveRight_f );
     CMD_Register( "moveDown", MoveDown_f );
     CMD_Register( "rotate", Rotate_f );
     CMD_Register( "restartGame", Restart_f );
-    I_Bind( "Left", "+moveLeft" );
-    I_Bind( "Right", "+moveRight" );
+    CMD_Register( "horizontalMove", HorzAxis_f );
+    I_Bind( "Left", "moveLeft" );
+    I_Bind( "Right", "moveRight" );
     I_Bind( "Down", "moveDown" );
     I_Bind( "Up", "+rotate" );
     I_Bind( "Space", "+restartGame" );
+    I_Bind( "joystick 0 axis 0", "horizontalMove" );
 }
 
 static void AppFrame( void ) {
@@ -562,6 +632,9 @@ static void AppFrame( void ) {
     GameUpdate();
     if ( VAR_Num( x_showAtlas ) ) {
         DrawAtlas();
+    }
+    if ( VAR_Changed( x_musicVolume ) ) {
+        UpdateMusicVolume();
     }
     SDL_Delay( 10 );
 }
